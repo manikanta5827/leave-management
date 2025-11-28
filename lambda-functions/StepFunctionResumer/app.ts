81ad10f8c3e778a95f12642a2a59a404004fc751
Manikanta Thummuri <postbox5827@gmail.com>
2025-11-28 23:34:31 +0530

    update: handle failure responses

--- lambda-functions/StepFunctionResumer/app.ts
+++ lambda-functions/StepFunctionResumer/app.ts
@@ -9,40 +9,58 @@ import { sfnClient } from "../Shared/step-function/client";
 export const handler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  const { requestId, status } = event.queryStringParameters as {
-    requestId: string;
-    status: string;
-  };
+  try {
+    const { requestId, status } = event.queryStringParameters as {
+      requestId: string;
+      status: string;
+    };
 
-  // fetch token from Token table using requestId
-  const data = await Token.get({ requestId });
-  const token = data.token;
+    // fetch token from Token table using requestId
+    const data = await Token.get({ requestId });
+    const token = data.token;
+    const userEmail = data.userEmail;
 
-  if (status == "accept") {
-    const command = new SendTaskSuccessCommand({
-      taskToken: token,
-      output: JSON.stringify({
-        status: "APPROVED",
-        data: "accept",
+    console.log("status:: ", status);
+    if (status == "accept") {
+      const command = new SendTaskSuccessCommand({
+        taskToken: token,
+        output: JSON.stringify({
+          status: "APPROVED",
+          data: {
+            userEmail: userEmail,
+          },
+        }),
+      });
+
+      await sfnClient.send(command);
+    } else {
+      const command = new SendTaskFailureCommand({
+        taskToken: token,
+        error: "ApprovalRejected",
+        cause: JSON.stringify({
+          reason: "Admin rejected the request",
+          userEmail,
+        }),
+      });
+
+      await sfnClient.send(command);
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Status send",
+        time: new Date().toISOString(),
       }),
-    });
-
-    await sfnClient.send(command);
-  } else {
-    const command = new SendTaskFailureCommand({
-      taskToken: token,
-      error: "ApprovalRejected",
-      cause: JSON.stringify({ reason: "Admin rejected the request" }),
-    });
-
-    await sfnClient.send(command);
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Something went wrong",
+        time: new Date().toISOString(),
+      }),
+    };
   }
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Status send",
-      time: new Date().toISOString(),
-    }),
-  };
 };
