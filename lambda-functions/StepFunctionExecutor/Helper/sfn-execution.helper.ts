260eba8ba27d68965dbd1cb6d85af50a09cd80da
Manikanta Thummuri <postbox5827@gmail.com>
2025-11-28 18:32:14 +0530

    feat: implement startStepFunctionExecution helper for initiating Step Function executions

---
+++ lambda-functions/StepFunctionExecutor/Helper/sfn-execution.helper.ts
@@ -0,0 +1,18 @@
+import {
+  StartExecutionCommand,
+  StartExecutionCommandOutput,
+} from "@aws-sdk/client-sfn";
+import { sfnClient } from "../Config/sfn-client.config";
+
+export const startStepFunctionExecution = async (
+  stateMachineArn: string,
+  body: any
+): Promise<StartExecutionCommandOutput> => {
+  const command = new StartExecutionCommand({
+    stateMachineArn: stateMachineArn,
+    input: JSON.stringify(body ? body : {}),
+  });
+
+  const result = await sfnClient.send(command);
+  return result;
+};
