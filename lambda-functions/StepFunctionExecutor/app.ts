fe673291efbbcc56a7a5b52bde8faa4b1e7c61d2
Manikanta Thummuri <postbox5827@gmail.com>
2025-11-28 18:33:24 +0530

    feat: enhance Step Function Executor Lambda to validate input and refactor execution logic

--- lambda-functions/StepFunctionExecutor/app.ts
+++ lambda-functions/StepFunctionExecutor/app.ts
@@ -1,21 +1,50 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import { SFNClient, StartExecutionCommand } from "@aws-sdk/client-sfn";
+import { startStepFunctionExecution } from "./Helper/sfn-execution.helper";
+import { InputPayload } from "./Types/input-request.types";
 
-const client = new SFNClient({});
+const STATE_MACHINE_ARN = process.env.STATE_MACHINE_ARN;
+
+if (!STATE_MACHINE_ARN)
+  throw new Error("STATE_MACHINE_ARN is not passed in env");
 
 export const handler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
   try {
-    console.log(
-      `step function executor lambda triggered ${new Date().toDateString()}`
-    );
-    const command = new StartExecutionCommand({
-      stateMachineArn: process.env.STATE_MACHINE_ARN,
-      input: JSON.stringify(event.body ? JSON.parse(event.body) : {}),
-    });
+    let body = JSON.parse(event.body as string);
+    body.apiUrl = `https://${event.requestContext.domainName}/${event.requestContext.stage}`;
 
-    const result = await client.send(command);
+    const { email, username, noOfDays, reason } = body as InputPayload;
+
+    if (!email) {
+      return sendResponse(400, {
+        status: "error",
+        message: "email is required",
+      });
+    }
+
+    if (!username) {
+      return sendResponse(400, {
+        status: "error",
+        message: "username is required",
+      });
+    }
+
+    if (!noOfDays || noOfDays < 0) {
+      return sendResponse(400, {
+        status: "error",
+        message: "noOfDays is required",
+      });
+    }
+
+    if (!reason) {
+      return sendResponse(400, {
+        status: "error",
+        message: "reason is required",
+      });
+    }
+
+    const result = await startStepFunctionExecution(STATE_MACHINE_ARN, body);
 
     return {
       statusCode: 200,
@@ -26,12 +55,19 @@ export const handler = async (
     };
   } catch (err) {
     console.error(err);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        status: "failed",
-        message: "Step function execution failed",
-      }),
-    };
+    return sendResponse(500, {
+      status: "failed",
+      message: "Step function triggering failed",
+    });
   }
 };
+
+function sendResponse(
+  statusCode: number,
+  body: { status: string; message: string }
+) {
+  return {
+    statusCode: statusCode,
+    body: JSON.stringify(body),
+  };
+}
