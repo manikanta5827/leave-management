7c2d779b81f007cb7e2d2638af08895b65101ec6
Manikanta Thummuri <postbox5827@gmail.com>
2025-11-28 18:31:05 +0530

    feat: add DynamoDB configuration

---
+++ lambda-functions/LeaveApproval/Config/dynamodb.config.ts
@@ -0,0 +1,21 @@
+import dynamoose from "dynamoose";
+
+const TokenSchema = new dynamoose.Schema({
+  requestId: {
+    type: String,
+    hashKey: true,
+  },
+  token: String,
+  userEmail: String,
+  noOfDays: Number,
+  username: String,
+  reason: String
+});
+
+const TOKEN_TABLE = process.env.TOKEN_TABLE;
+
+if (!TOKEN_TABLE) throw new Error("TOKEN_TABLE not passed in env");
+
+const Token = dynamoose.model(TOKEN_TABLE, TokenSchema, { create: false });
+
+export default Token;
