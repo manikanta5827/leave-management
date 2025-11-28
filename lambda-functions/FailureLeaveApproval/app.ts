a8b9252901078bcedbf360e8040090f4d70c42ca
Manikanta Thummuri <postbox5827@gmail.com>
2025-11-29 00:27:49 +0530

    update: send notifications for success and failure leave approvals

---
+++ lambda-functions/FailureLeaveApproval/app.ts
@@ -0,0 +1,23 @@
+import { sendEmail } from "../Shared/ses/email-sender.helper";
+
+const PROJECT_EMAIL = process.env.PROJECT_EMAIL;
+
+if (!PROJECT_EMAIL) throw new Error("PROJECT_EMAIL is not passes in env");
+
+export const handler = async (event: {
+  Error: string;
+  Cause: string;
+}): Promise<{}> => {
+  const { userEmail } = JSON.parse(event.Cause) as {
+    reason: string;
+    userEmail: string;
+  };
+
+  await sendEmail(
+    PROJECT_EMAIL,
+    userEmail,
+    `Leave Approval Status`,
+    `Leave approval is Rejected, Bettwe luck next Time..:)`
+  );
+  return {};
+};
---
+++ lambda-functions/SuccessLeaveApproval/app.ts
@@ -0,0 +1,19 @@
+import { sendEmail } from "../Shared/ses/email-sender.helper";
+
+const PROJECT_EMAIL = process.env.PROJECT_EMAIL;
+
+if (!PROJECT_EMAIL) throw new Error("PROJECT_EMAIL is not passes in env");
+
+export const handler = async (event: {
+  status: string;
+  data: { userEmail: string };
+}): Promise<{}> => {
+  const userEmail = event.data.userEmail;
+  await sendEmail(
+    PROJECT_EMAIL,
+    userEmail,
+    `Leave Approval Status`,
+    `Leave approval is Accepted, enjoy..:)`
+  );
+  return {};
+};
