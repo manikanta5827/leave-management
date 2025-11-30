f26a586ebbacd3e087a5e0e74e54f9a05b14a99d
Manikanta Thummuri <postbox5827@gmail.com>
2025-11-30 23:05:29 +0530

    update: add project and admin email environment variables

--- .jest/env-setup.ts
+++ .jest/env-setup.ts
@@ -1,2 +1,4 @@
 process.env.SECRET = "somesecret";
 process.env.STATE_MACHINE_ARN = "some arn";
+process.env.PROJECT_EMAIL = "thummurimanikanta7@gmail.com";
+process.env.ADMIN_EMAIL = "postbox5827@gmail.com";
