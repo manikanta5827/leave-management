5b1a24986567e418dc46bee01ece903878e59125
Manikanta Thummuri <postbox5827@gmail.com>
2025-11-30 20:50:01 +0530

    update: add step function executor tests

--- tests/unit/step-fn-executor.test.ts
+++ tests/unit/step-fn-executor.test.ts
@@ -6,7 +6,7 @@ import { SFNClient, StartExecutionCommand } from "@aws-sdk/client-sfn";
 
 import { readFileSync } from "node:fs";
 import path from "node:path";
-const event = JSON.parse(
+const event: APIGatewayProxyEvent = JSON.parse(
   readFileSync(
     path.join(__dirname + "/../events/step-fn-executor.event.json"),
     "utf8"
@@ -22,10 +22,29 @@ describe("Test step fn executor lambda", () => {
       executionArn: "some-arn",
     });
 
-    let result: APIGatewayProxyResult = await handler(
-      event as APIGatewayProxyEvent
-    );
+    let result: APIGatewayProxyResult = await handler(event);
 
-    console.log(result);
+    expect(result.statusCode).toBe(200);
+
+    let body = JSON.parse(result.body);
+    expect(body.executionArn).toBeDefined();
+  });
+
+  it("return 400 for no username", async () => {
+    sfnMock.on(StartExecutionCommand).resolves({
+      executionArn: "some-arn",
+    });
+
+    let body = JSON.parse(event.body as string);
+    delete body.username;
+    event.body = JSON.stringify(body);
+
+    let result: APIGatewayProxyResult = await handler(event);
+
+    expect(result.statusCode).toBe(400);
+
+    body = JSON.parse(result.body);
+    console.log(body);
+    expect(body.message).toBe("username is required");
   });
 });
