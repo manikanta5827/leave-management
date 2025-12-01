64f269b8b0fd7b6ab0b38a63e0835a19e13e5725
Manikanta Thummuri <postbox5827@gmail.com>
2025-12-01 17:41:28 +0530

    update: getSecret function to accept secret name and key

--- lambda-functions/Shared/secrets-manager/helper/secrets-getter.ts
+++ lambda-functions/Shared/secrets-manager/helper/secrets-getter.ts
@@ -2,22 +2,29 @@ import secretsClient from "../config/client";
 import { GetSecretValueCommand } from "@aws-sdk/client-secrets-manager";
 let cache: Map<string, string> = new Map();
 
-export const getSecret = async (secret_key: string): Promise<string> => {
+export const getSecret = async (
+  secret_name: string,
+  secret_key: string
+): Promise<string> => {
   try {
     // get the secret from cache
-    let secret = cache.get(secret_key);
+    let secret = cache.get(secret_name);
 
     // if not present in cache then get it from source
     if (!secret) {
       let response = await secretsClient.send(
         new GetSecretValueCommand({
-          SecretId: secret_key,
+          SecretId: secret_name,
         })
       );
       // if secret is defined then store it in cache
       if (response.SecretString) {
-        cache.set(secret_key, response.SecretString);
-        return response.SecretString;
+        let secret = JSON.parse(response.SecretString) as {
+          secret_key: string;
+        };
+
+        cache.set(secret_name, secret.secret_key);
+        return secret.secret_key;
       }
       throw new Error(`${secret_key} not found`);
     }
